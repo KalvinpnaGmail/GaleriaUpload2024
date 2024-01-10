6a464e2cd78f84041a678cc26d806c330e493ee4
Ramón Gabriel López <lopez.gabriel@outlook.com>
2024-01-10 11:04:15 -0300

    JSON CREADON EN CARPETA JSON

---
+++ UPLOAD.MOBILE/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UPLOAD.MOBILE
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
