da14a3853d9b6f04188d79f179e8abb1e990aab5
Ramón Gabriel López <lopez.gabriel@outlook.com>
2024-06-27 15:40:12 -0300

    Sa

---
+++ UPLOAD.WEB/Pages/Countries/CountryIndex.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using UPLOAD.SHARE.Entities;
+using UPLOAD.WEB.Repositories;
+
+namespace UPLOAD.WEB.Pages.Countries
+{
+    public partial class CountryIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        public List<Country>? Countries { set; get; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await Repository.GetAsync<List<Country>>("/api/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
