86c13bcffb50c89ead25a7d9b2f361e7a57ca0c2
Ramón Gabriel López <lopez.gabriel@outlook.com>
2024-07-22 09:33:44 -0300

    Reapply "Repositorios y Unidades de trabajo genéricas"
    This reverts commit 8093097ffdbd699195643e385a444efb695cd8a2.

--- UPLOAD.API/Program.cs
+++ UPLOAD.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using UPLOAD.API.Data;
+using UPLOAD.API.Repositories.Implementations;
+using UPLOAD.API.Repositories.Interfaces;
+using UPLOAD.API.UnitsOfWork.Implementations;
+using UPLOAD.API.UnitsOfWork.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -31,6 +35,11 @@ builder.Services.AddDbContext<DataContext>(options =>
         (a) => a.MigrationsAssembly("UPLOAD.API"));
 },
 ServiceLifetime.Transient);
+builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWork<>));
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+
+
 //builder.Services.AddCors();
 var app = builder.Build();
 
