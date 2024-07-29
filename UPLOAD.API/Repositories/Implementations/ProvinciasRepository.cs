75719a0a606040a6250a432656d59e7b69202fb0
Ramón Gabriel López <lopez.gabriel@outlook.com>
2024-07-29 14:52:47 -0300

    Tutorial 72 - Parte 22 - Repositorio de ciudades

---
+++ UPLOAD.API/Repositories/Implementations/ProvinciasRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UPLOAD.API.Data;
+using UPLOAD.API.Repositories.Interfaces;
+using UPLOAD.SHARE.Entities;
+using UPLOAD.SHARE.Response;
+
+namespace UPLOAD.API.Repositories.Implementations
+{
+    public class ProvinciasRepository : GenericRepository<Provincia>, IProvinciasRepository
+    {
+        private readonly DataContext _contex;
+
+        public ProvinciasRepository(DataContext contex) : base(contex)
+        {
+           _contex = contex;
+        }
+
+        public override async Task<ActionResponse<Provincia>> GetAsync(int id)
+        {
+           
+            var provincia = await _contex.Provincias
+                .Include(c => c.Cities)
+                .FirstOrDefaultAsync(c => c.Id == id);  
+            if (provincia==null)
+            {
+                return new ActionResponse<Provincia>
+                {
+                    WasSuccess = false,
+                    Message = "Provincia no Existe"
+                };
+            }
+            return new ActionResponse<Provincia>
+            {
+                WasSuccess = true,
+                Result = provincia
+            };
+        }
+
+        public override async Task<ActionResponse<IEnumerable<Provincia>>> GetAsync()
+        {
+            var provincias = await _contex.Provincias.Include(c => c.Cities).ToListAsync();
+            return new ActionResponse<IEnumerable<Provincia>>
+            {
+                WasSuccess = true,
+                Result = provincias
+            };
+
+        }
+    }
+}
