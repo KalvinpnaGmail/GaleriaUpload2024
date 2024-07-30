5f7333ed7b8e851ae7151877ab1e73fe2087ca7f
kalvinpna <lopez.gabriel@outlook.com>
2024-07-30 14:11:54 -0300

    Tutorial 72 - Parte 24 Crear Estado

--- UPLOAD.WEB/Pages/Provincias/ProvinciasCreate.razor.cs
+++ UPLOAD.WEB/Pages/Provincias/ProvinciasCreate.razor.cs
@@ -1,6 +1,59 @@
-﻿namespace UPLOAD.WEB.Pages.Provincias
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using UPLOAD.SHARE.Entities;
+using UPLOAD.WEB.Repositories;
+using UPLOAD.WEB.Shared;
+
+namespace UPLOAD.WEB.Pages.Provincias
 {
     public partial class ProvinciasCreate
     {
+        private Provincia provincia = new();
+
+
+        /// para referencias la no navegacion 
+        private FormWihtName<Provincia>? provinciaForm;
+
+        [Parameter] public int CountryId { get; set; }
+
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        public async Task CreateAsync()
+        {  ///solo se ejecute cuando el formualario sea valido que paso las validaciones
+            provincia.CountryId = CountryId;
+            var responseHttp = await Repository.PostAsync("/api/provincias", provincia);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+            Return();
+
+            ///si paso las validaciones
+            ///tostadita abajo y final informativo se usa tostadita
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Provincia creada con éxito.");
+
+        }
+
+
+
+
+        private void Return()
+        {
+            //si lo grabo
+            provinciaForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo($"/countries/details/{CountryId}");
+        }
     }
 }
---
+++ UPLOAD.WEB/Pages/Provincias/ProvinciasEdit.razor.cs
@@ -0,0 +1,31 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using UPLOAD.SHARE.Entities;
+using UPLOAD.WEB.Repositories;
+using UPLOAD.WEB.Shared;
+
+namespace UPLOAD.WEB.Pages.Provincias
+{
+    public partial class ProvinciasEdit
+    {
+        private Provincia? provincia;
+
+        /// para referencias la no navegacion  <summary>
+        /// para poder cambiar la propiedad FormPostedSuccessfully
+        private FormWihtName<Provincia>? provinciaForm;
+
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+
+        ////pasame id como parametro para ver que provincia es
+        [EditorRequired, Parameter] public int CountryId { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+
+    }
+}
