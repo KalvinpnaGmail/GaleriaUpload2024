3f27a6a6f507de46eb5647d05e321a8d11d2fd0f
Ramón Gabriel López <lopez.gabriel@outlook.com>
2024-09-20 08:11:24 -0300

    limpieza de librerias

--- UPLOAD.SHARE/Entities/City.cs
+++ UPLOAD.SHARE/Entities/City.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 using UPLOAD.SHARE.Interfaces;
 
 namespace UPLOAD.SHARE.Entities
 {
-    public class City :IEntityWithName
+    public class City : IEntityWithName
     {
         public int Id { get; set; }
 
