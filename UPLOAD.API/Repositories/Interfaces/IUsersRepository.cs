0cf6a9d21d477d1120f136656c522b28a8290f82
Ramón Gabriel López <lopez.gabriel@outlook.com>
2024-09-27 14:11:16 -0300

    editar usuario en login...video 51

--- UPLOAD.API/Repositories/Interfaces/IUsersRepository.cs
+++ UPLOAD.API/Repositories/Interfaces/IUsersRepository.cs
@@ -23,5 +23,14 @@ namespace UPLOAD.API.Repositories.Interfaces
         //desloguearse
         Task LogoutAsync();
 
+
+        Task<User> GetUserAsync(Guid userId);
+
+        Task<IdentityResult> ChangePasswordAsync(User user, string currentPassword, string newPassword);
+
+        Task<IdentityResult> UpdateUserAsync(User user);
+
+
+
     }
 }
